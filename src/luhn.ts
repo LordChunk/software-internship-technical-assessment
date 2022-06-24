9998d3b7039f87792911184d4b0d86187b53abd9
Job <LordChunk@users.noreply.github.com>
2022-06-24 14:03:02 +0200

    Removed some unnecessary comments

--- src/luhn.ts
+++ src/luhn.ts
@@ -11,18 +11,15 @@ const luhn = (rawInput: string): void => {
 
   const inputList = input.split('').map(Number);
 
-  // Do not accept single digit numbers
   if (inputList.length === 1) {
     exit(42, 'Input cannot be a single digit');
   }
 
   let totalSum = 0;
   // Perform Luhn algorithm
-  // flip array
   inputList.reverse().forEach((value, index) => {
     if(index % 2 !== 0) {
       let doubled = value * 2;
-      // If doubled is greater than 9, subtract 9
       if (doubled > 9) {
         totalSum -= 9;
       }      
@@ -47,6 +44,5 @@ const exit = (exitCode: number, message?: string) => {
   console.log('Exiting with code:', exitCode);
   process.exit(exitCode);
 }
-
 
 export default luhn;
